fdbc58cb026d5e85af96d15cc3358cae9e4578f2
Asd396 <leokotipalo@gmail.com>
2020-02-22 19:47:02 +0200

    Async, 5.1

--- osa5/bloglist-frontend/src/App.js
+++ osa5/bloglist-frontend/src/App.js
@@ -1,22 +1,50 @@
 import React, { useState, useEffect } from 'react'
-import Blog from './components/Blog'
+import Blogs from './components/Blogs'
 import blogService from './services/blogs'
+import loginService from './services/login'
+import LoginForm from './components/LoginForm'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
+  const [user, setUser] = useState(null)
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
 
   useEffect(() => {
-    blogService.getAll().then(blogs =>
-      setBlogs( blogs )
-    )  
+    async function getBlogs() {
+      const blogs = await blogService.getAll()
+      setBlogs(blogs)
+    }
+
+    getBlogs()
   }, [])
+
+  const handleLogin = async (event) => {
+    event.preventDefault()
+    console.log('logging in with', username, password)
+
+    try {
+      const user = await loginService.login({
+        username, password,
+      })
+
+      setUser(user)
+      setUsername('')
+      setPassword('')
+    } catch (exception) {
+      console.log('login failed')
+    }
+  }
 
   return (
     <div>
-      <h2>blogs</h2>
-      {blogs.map(blog =>
-        <Blog key={blog.id} blog={blog} />
-      )}
+      {user ? <Blogs blogs={blogs}/> :
+        <LoginForm username = {username} 
+          password = {password} 
+          setUsername = {setUsername} 
+          setPassword={setPassword}
+          onSubmit={handleLogin}/>
+      }
     </div>
   )
 }
