9f1a7c5bf511d3f97da6ba2934f4c3b9bd6b4492
Asd396 <leokotipalo@gmail.com>
2020-03-02 19:52:28 +0200

    6.15

--- osa6/redux-anecdotes/src/App.js
+++ osa6/redux-anecdotes/src/App.js
@@ -3,18 +3,13 @@ import AnecdoteList from './components/AnecdoteList'
 import AnecdoteForm from './components/AnecdoteForm'
 import Notification from './components/Notification'
 import Filter from './components/Filter'
-import anecdoteService from './services/anecdotes.js'
 import { initializeAnecdotes } from './reducers/anecdoteReducer'
 import { useDispatch } from 'react-redux'
 
 const App = () => {
   const dispatch = useDispatch()
   useEffect(() => {
-    const getAnecdotes = async () => {
-      const anecdotes = await anecdoteService.getAll()
-      dispatch(initializeAnecdotes(anecdotes))
-    }
-    getAnecdotes()
+    dispatch(initializeAnecdotes())
   }, [dispatch])
 
   return (
