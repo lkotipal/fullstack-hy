35152e2cffbd6804c0bb622dfdf994e8629f5304
Asd396 <leokotipalo@gmail.com>
2020-03-10 19:51:51 +0200

    7.8

--- osa7/ultimate-hooks/src/App.js
+++ osa7/ultimate-hooks/src/App.js
@@ -20,10 +20,19 @@ const useField = (type) => {
 const useResource = (baseUrl) => {
   const [resources, setResources] = useState([])
 
-  // ...
+  useEffect(() => {
+    const getResources = async () => {
+      const response = await axios.get(baseUrl)
+      setResources(response.data)
+    }
 
-  const create = (resource) => {
-    // ...
+    getResources()
+  }, [baseUrl])
+
+  const create = async (resource) => {
+    const response = await axios.post(baseUrl, resource)
+    setResources(resources.concat(response.data))
+    return response.data
   }
 
   const service = {
