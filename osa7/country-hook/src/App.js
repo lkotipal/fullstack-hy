c9a935291b1c7a55357ae6acb1f77a27700b9578
Asd396 <leokotipalo@gmail.com>
2020-03-10 18:44:22 +0200

    7.7

--- osa7/country-hook/src/App.js
+++ osa7/country-hook/src/App.js
@@ -18,7 +18,26 @@ const useField = (type) => {
 const useCountry = (name) => {
   const [country, setCountry] = useState(null)
 
-  useEffect()
+
+  useEffect(() => {
+    const getCountry = async (name) => {
+      try {
+        console.log(name)
+        const response = await axios.get(`https://restcountries.eu/rest/v2/name/${name}?fullText=true`)
+        setCountry({...response.data[0], found: true})
+      } catch(e) {
+        if (e.response.status === 404) {
+          setCountry({found: false})
+        } else {
+          throw e
+        }
+      }
+    }
+
+    if (name) {
+      getCountry(name)
+    }
+  }, [name])
 
   return country
 }
@@ -38,10 +57,10 @@ const Country = ({ country }) => {
 
   return (
     <div>
-      <h3>{country.data.name} </h3>
-      <div>capital {country.data.capital} </div>
-      <div>population {country.data.population}</div> 
-      <img src={country.data.flag} height='100' alt={`flag of ${country.data.name}`}/>  
+      <h3>{country.name} </h3>
+      <div>capital {country.capital} </div>
+      <div>population {country.population}</div> 
+      <img src={country.flag} height='100' alt={`flag of ${country.name}`}/>  
     </div>
   )
 }
