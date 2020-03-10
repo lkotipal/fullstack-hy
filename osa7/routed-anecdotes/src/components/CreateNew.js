f50e0f1f32b000331209472c0681ef81e69de368
Asd396 <leokotipalo@gmail.com>
2020-03-10 17:53:01 +0200

    7.6 BROKEN

--- osa7/routed-anecdotes/src/components/CreateNew.js
+++ osa7/routed-anecdotes/src/components/CreateNew.js
@@ -17,23 +17,33 @@ const CreateNew = (props) => {
     })
   }
 
+  const handleReset = (e) => {
+    e.preventDefault()
+    content.reset()
+    author.reset()
+    info.reset()
+  }
+
+  const WrappedInput = ({reset, ...props}) => <input {...props} />
+
   return (
     <div>
       <h2>create a new anecdote</h2>
       <form onSubmit={handleSubmit}>
         <div>
           content
-          <input name='content' {...content} />
+          <WrappedInput name='content' {...content} />
         </div>
         <div>
           author
-          <input name='author' {...author} />
+          <WrappedInput name='author' {...author} />
         </div>
         <div>
           url for more info
-          <input name='info' {...info} />
+          <WrappedInput name='info' {...info} />
         </div>
         <button>create</button>
+        <button onClick={handleReset}>reset</button>
       </form>
     </div>
   )
