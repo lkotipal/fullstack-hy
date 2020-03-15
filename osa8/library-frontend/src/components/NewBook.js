5a351e3855212a7fe6dd6331c5c72584d84993b9
Asd396 <leokotipalo@gmail.com>
2020-03-15 03:54:07 +0200

    8.10

--- osa8/library-frontend/src/components/NewBook.js
+++ osa8/library-frontend/src/components/NewBook.js
@@ -1,11 +1,17 @@
 import React, { useState } from 'react'
+import { useMutation } from '@apollo/client'
+import { ALL_AUTHORS, ALL_BOOKS, CREATE_BOOK } from '../queries'
 
 const NewBook = (props) => {
   const [title, setTitle] = useState('')
-  const [author, setAuhtor] = useState('')
+  const [author, setAuthor] = useState('')
   const [published, setPublished] = useState('')
   const [genre, setGenre] = useState('')
   const [genres, setGenres] = useState([])
+
+  const [ createBook ] = useMutation(CREATE_BOOK, {
+    refetchQueries: [{ query: ALL_AUTHORS }, { query: ALL_BOOKS }]  
+  })
 
   if (!props.show) {
     return null
@@ -14,11 +20,11 @@ const NewBook = (props) => {
   const submit = async (event) => {
     event.preventDefault()
     
-    console.log('add book...')
+    console.log(createBook({ variables: { title, published: parseInt(published), author, genres } }))
 
     setTitle('')
     setPublished('')
-    setAuhtor('')
+    setAuthor('')
     setGenres([])
     setGenre('')
   }
@@ -42,7 +48,7 @@ const NewBook = (props) => {
           author
           <input
             value={author}
-            onChange={({ target }) => setAuhtor(target.value)}
+            onChange={({ target }) => setAuthor(target.value)}
           />
         </div>
         <div>
---
+++ osa8/library-frontend/src/queries.js
@@ -0,0 +1,39 @@
+import { gql } from '@apollo/client'
+
+export const ALL_AUTHORS = gql`
+  query {
+    allAuthors  {
+      name
+      id
+      born
+      bookCount
+    }
+  }
+`
+
+export const ALL_BOOKS = gql`
+  query {
+    allBooks  {
+      title
+      id
+      author
+      published
+    }
+  }
+`
+
+export const CREATE_BOOK = gql`
+  mutation createBook($title: String!, $published: Int!, $author: String!, $genres: [String!]!) {
+    addBook(
+      title: $title
+      published: $published
+      author: $author
+      genres: $genres
+    ) {
+      title
+      published
+      author
+      genres
+    }
+  }
+`
